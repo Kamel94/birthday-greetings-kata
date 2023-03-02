be712f2302e4cbda25f3316ce680ca6a2d5ecb17
Kamel <a.kamel.square@gmail.com>
2023-03-02 11:01:54 +0100

    feat: déplacement de la méthode sendMessage dans la classe MailSender.

---
+++ src/main/java/it/xpug/kata/birthday_greetings/MailSender.java
@@ -0,0 +1,38 @@
+package it.xpug.kata.birthday_greetings;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailSender {
+
+    private final String smtpHost;
+    private final int smtpPort;
+
+    public MailSender(String smtpHost, int smtpPort) {
+        this.smtpHost = smtpHost;
+        this.smtpPort = smtpPort;
+    }
+
+    void sendMessage(String sender, String subject, String body, String recipient) throws AddressException, MessagingException {
+        // Create a mail session
+        java.util.Properties props = new java.util.Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", "" + smtpPort);
+        Session session = Session.getInstance(props, null);
+
+        // Construct the message
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(sender));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        // Send the message
+        Transport.send(msg);
+    }
+}
